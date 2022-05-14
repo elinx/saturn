fb9b17a1f59084b9709c48c0571871df8cfa49e0
Elinx Hsi <xizzuli@163.com>
2022-05-14 22:48:37 +0800

    display toc in bubletea

--- cmd/saturn/model.go
+++ cmd/saturn/model.go
@@ -1,18 +1,43 @@
 package main
 
 import (
+	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/elinx/saturn/pkg/epub"
 )
 
+type item struct {
+	title string
+}
+
+func (i item) FilterValue() string { return i.title }
+func (i item) Title() string       { return i.title }
+func (i item) Description() string { return "" }
+
+func newItems(book *epub.Epub) []list.Item {
+	content := []list.Item{}
+	for _, v := range book.Toc.NavMap.NavPoints {
+		content = append(content, item{v.NavLable.Text})
+		for _, v := range v.NavPoints {
+			content = append(content, item{"  " + v.NavLable.Text})
+		}
+	}
+	return content
+}
+
 func NewModel(book *epub.Epub) tea.Model {
 	return model{
 		book: book,
+		list: list.New(newItems(book), list.DefaultDelegate{
+			ShowDescription: false,
+			Styles:          list.NewDefaultItemStyles(),
+		}, 30, 30),
 	}
 }
 
 type model struct {
 	book *epub.Epub
+	list list.Model
 }
 
 func (m model) Init() tea.Cmd {
@@ -27,16 +52,12 @@ func (m model) Update(message tea.Msg) (tea.Model, tea.Cmd) {
 			return m, tea.Quit
 		}
 	}
-	return m, nil
+
+	var cmd tea.Cmd
+	m.list, cmd = m.list.Update(message)
+	return m, cmd
 }
 
 func (m model) View() string {
-	content := ""
-	for _, v := range m.book.Toc.NavMap.NavPoints {
-		content += v.NavLable.Text + "\n"
-		for _, v := range v.NavPoints {
-			content += "  " + v.NavLable.Text + "\n"
-		}
-	}
-	return content
+	return m.list.View()
 }
